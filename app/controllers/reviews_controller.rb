24380fceb3bfc26b1085578d775575c4508dc67c
javier echeverri <jecheverrid@gmail.com>
2020-05-21 17:43:50 +0200

    all views controllers and test implemented correctly and passed successfully

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,31 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant, only: [:new, :create]
+  def new
+    @review = Review.new
+    # raise
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+
+
+  end
+
+  private
+
+  def review_params
+     params.require(:review).permit(:rating, :content)
+  end
+
+  def find_restaurant
+     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+
 end
