4d14b0a1232dd011b9e516010a079c43c374717f
javier echeverri <jecheverrid@gmail.com>
2020-05-21 16:30:02 +0200

    new and show controllers and views implemented

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,47 @@
 class RestaurantsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show]
+
   def index
     @restaurants = Restaurant.all
     @review_hash = {}
     @restaurants.each do |restaurant|
       rating = 0
       restaurant.reviews.each { |review| rating += review.rating }
-      @review_hash[restaurant.id] = rating.div(restaurant.reviews.count)
+      if restaurant.reviews.empty?
+        @review_hash[restaurant.id] = 0
+      else
+        @review_hash[restaurant.id] = rating.div(restaurant.reviews.count)
+      end
     end
     @review_hash = @review_hash.sort_by { |_key, value| -value }.to_h
   end
 
   def show
+    @rating = 0
+    @restaurant.reviews.each { |review| @rating += review.rating }
+    @rating /= @restaurant.reviews.count unless @restaurant.reviews.empty?
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number,:category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+
 end
