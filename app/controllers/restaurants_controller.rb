49fc565cc74775d64009967d451983ab142c2596
javier echeverri <jecheverrid@gmail.com>
2020-05-21 11:46:15 +0200

    get view implemented, link to show implemented

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,15 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    @review_hash = {}
+    @restaurants.each do |restaurant|
+      rating = 0
+      restaurant.reviews.each { |review| rating += review.rating }
+      @review_hash[restaurant.id] = rating.div(restaurant.reviews.count)
+    end
+    @review_hash = @review_hash.sort_by { |_key, value| -value }.to_h
+  end
+
+  def show
   end
 end
